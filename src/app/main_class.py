f549c3588fcfa3b19eb02ae84edb3f03bcd97dab
Yakubov, Sergey <yakubovs@ornl.gov>
2024-10-04 10:55:35 -0400

    update format

--- src/app/main_class.py
+++ src/app/main_class.py
@@ -4,8 +4,20 @@
 class MainClass:
     """Main Class."""
 
-    def name(self) -> str:
-        return "name"
+    def name(self, value: str) -> str:
+        """Example function with types documented in the docstring.
+
+        Parameters
+        ----------
+        value :str
+             The first parameter.
+
+        Returns
+        -------
+        str
+            The return value. Here just returns the function argument.
+        """
+        return value
 
 
 __all__ = ["MainClass"]
--- tests/test_main_class.py
+++ tests/test_main_class.py
@@ -5,4 +5,4 @@ from app import MainClass
 
 def test_version() -> None:
     app = MainClass()
-    assert app.name() == "name"
+    assert app.name("test") == "test"
